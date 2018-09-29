bc55c6b65fd566a3ed513cf15eeb5c8feeacc43a
sunshanghai <sunshanghai@liquidnetwork.com>
2018-09-29 12:01:32 +0800

    fix pom bug

--- configure.sh
+++ configure.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
 
 
-echo -e ‘\n\n\n\n\n’;
+echo -e ‘\n\n\n\n\n aliyun mirror’;
 sudo mv ~/.m2/settings.xml ~/.m2/settings.xml_bak
 cp -rfv ./settings.xml ~/.m2/settings.xml
 echo -e ‘\n\n\n\n\n’;
-
+sleep 2;
 
 cd spring-cloud-parent
 mvn clean install -DskipTests -U;
 echo -e ‘\n\n\n\n\n’;
-sleep 1;
+sleep 2;
 
 
 cd ../spring-cloud-client
 mvn clean install -DskipTests -U;
 echo -e ‘\n\n\n\n\n’;
-sleep 1;
+sleep 2;
 
 
 cd ../spring-cloud-starter
 mvn clean install -DskipTests -U;
 echo -e ‘\n\n\n\n\n’;
-sleep 1;
+sleep 2;
 
 
 
 echo -e ‘\n\n\n\n\n’;
-echo "starting..................... redis”;
+echo -e ‘starting..................... redis’;
 
 sudo redis-cli -p 6379 shutdown;
 sleep 1;
