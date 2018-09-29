68d65dc6cf224298a9176c55484725c58e7971fc
sunshanghai <sunshanghai@liquidnetwork.com>
2018-09-29 11:50:18 +0800

    update spring-cloud-openfeign

--- configure.sh
+++ configure.sh
@@ -1,32 +1,40 @@
 #!/bin/sh
 
 
-
+echo -e ‘\n\n\n\n\n’;
+sudo mv ~/.m2/settings.xml ~/.m2/settings.xml_bak
+cp -rfv ./settings.xml ~/.m2/settings.xml
+echo -e ‘\n\n\n\n\n’;
 
 
 cd spring-cloud-parent
-mvn -U clean install -DskipTests
+mvn clean install -DskipTests -U;
+echo -e ‘\n\n\n\n\n’;
 sleep 1;
+
 
 cd ../spring-cloud-client
-mvn -U clean install -DskipTests
+mvn clean install -DskipTests -U;
+echo -e ‘\n\n\n\n\n’;
 sleep 1;
+
 
 cd ../spring-cloud-starter
-mvn -U clean install -DskipTests
+mvn clean install -DskipTests -U;
+echo -e ‘\n\n\n\n\n’;
 sleep 1;
 
 
 
-echo -e "\n\n\n"
-echo "starting..................... redis"
+echo -e ‘\n\n\n\n\n’;
+echo "starting..................... redis”;
 
 sudo redis-cli -p 6379 shutdown;
 sleep 1;
 
 sudo cp -rfv redis/*.conf /opt/;
-
 sudo redis-server /opt/redis_6379.conf;
-
 sleep 1;
-echo '';
+echo -e ‘\n\n\n\n\n’;
+
+
