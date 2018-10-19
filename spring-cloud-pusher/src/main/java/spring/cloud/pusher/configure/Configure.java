7556ac4ebc8cc0c28b77814cee1cd56f0a917682
sunshanghai <sunshanghai@liquidnetwork.com>
2018-10-19 11:06:07 +0800

    introduce quartz

--- spring-cloud-pusher/src/main/java/spring/cloud/pusher/configure/Configure.java
+++ spring-cloud-pusher/src/main/java/spring/cloud/pusher/configure/Configure.java
@@ -1,13 +1,13 @@
 package spring.cloud.pusher.configure;
 
 
-import org.apache.catalina.filters.RemoteIpFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import spring.cloud.pusher.filter.LoginFilter;
-import spring.cloud.pusher.listener.MyHttpSessionListener;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
+import spring.cloud.pusher.quartz.XAdCampaign2RedisJob;
+import spring.cloud.pusher.quartz.XAdFeedbackJob;
 
 /**
  * @author shsun
@@ -15,33 +15,56 @@ import spring.cloud.pusher.listener.MyHttpSessionListener;
 @Configuration
 public class Configure {
 
-//    @Bean
-//    public ServletListenerRegistrationBean<MyHttpSessionListener> serssionListenerBean() {
-//        return new ServletListenerRegistrationBean<MyHttpSessionListener>(new MyHttpSessionListener());
-//    }
-
-    @Bean
-    public RemoteIpFilter remoteIpFilter() {
-        return new RemoteIpFilter();
+    /**
+     * 方法调用任务明细工厂Bean
+     */
+    @Bean(name = "myFirstExerciseJobBean")
+    public MethodInvokingJobDetailFactoryBean myFirstExerciseJobBean(XAdCampaign2RedisJob job) {
+        MethodInvokingJobDetailFactoryBean jobDetail = new MethodInvokingJobDetailFactoryBean();
+        jobDetail.setConcurrent(false); // 是否并发
+        jobDetail.setName("general-myFirstExerciseJob"); // 任务的名字
+        jobDetail.setGroup("general"); // 任务的分组
+        jobDetail.setTargetObject(job); // 被执行的对象
+        jobDetail.setTargetMethod("myJobBusinessMethod"); // 被执行的方法
+        return jobDetail;
     }
 
-    @Bean
-    public FilterRegistrationBean testFilterRegistration() {
-        // 配置无需过滤的路径或者静态资源，如：css，imgage等
-        StringBuffer excludedUriStr = new StringBuffer();
-        excludedUriStr.append("/login/*");
-        excludedUriStr.append(",");
-        excludedUriStr.append("/favicon.ico");
-        excludedUriStr.append(",");
-        excludedUriStr.append("/js/*");
+    /**
+     * 表达式触发器工厂Bean
+     */
+    @Bean(name = "myFirstExerciseJobTrigger")
+    public CronTriggerFactoryBean myFirstExerciseJobTrigger(@Qualifier("myFirstExerciseJobBean") MethodInvokingJobDetailFactoryBean bean) {
+        CronTriggerFactoryBean tigger = new CronTriggerFactoryBean();
+        tigger.setJobDetail(bean.getObject());
+        tigger.setCronExpression("0/10 * * * * ?"); // 什么是否触发，Spring Scheduler Cron表达式
+        tigger.setName("general-myFirstExerciseJobTrigger");
+        return tigger;
+    }
 
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new LoginFilter());
-        registration.addUrlPatterns("/*");
-        registration.addInitParameter("excludedUri", excludedUriStr.toString());
-        registration.setName("loginFilter");
-        registration.setOrder(1);
-        return registration;
+    /**
+     * 方法调用任务明细工厂Bean
+     */
+    @Bean(name = "mySecondExerciseJobBean")
+    public MethodInvokingJobDetailFactoryBean mySecondExerciseJobBean(XAdFeedbackJob job) {
+        MethodInvokingJobDetailFactoryBean jobDetail = new MethodInvokingJobDetailFactoryBean();
+        jobDetail.setConcurrent(false); // 是否并发
+        jobDetail.setName("general-mySecondExerciseJob"); // 任务的名字
+        jobDetail.setGroup("general"); // 任务的分组
+        jobDetail.setTargetObject(job); // 被执行的对象
+        jobDetail.setTargetMethod("myJobBusinessMethod"); // 被执行的方法
+        return jobDetail;
+    }
+
+    /**
+     * 表达式触发器工厂Bean
+     */
+    @Bean(name = "mySecondExerciseJobTrigger")
+    public CronTriggerFactoryBean mySecondExerciseJobTrigger(@Qualifier("mySecondExerciseJobBean") MethodInvokingJobDetailFactoryBean bean) {
+        CronTriggerFactoryBean tigger = new CronTriggerFactoryBean();
+        tigger.setJobDetail(bean.getObject());
+        tigger.setCronExpression("0/10 * * * * ?");
+        tigger.setName("general-mySecondExerciseJobTrigger");
+        return tigger;
     }
 }
 
---
+++ spring-cloud-pusher/src/main/java/spring/cloud/pusher/quartz/XAdCampaign2RedisJob.java
@@ -0,0 +1,25 @@
+package spring.cloud.pusher.quartz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ */
+@Component
+@EnableScheduling
+public class XAdCampaign2RedisJob implements Job {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(XAdCampaign2RedisJob.class);
+
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        LOGGER.info("XAdCampaign2RedisJob");
+    }
+}
