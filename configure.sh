d263ce65a4f5d89207d3bfcb1709e92ee185eaf8
sunshanghai <sunshanghai@liquidnetwork.com>
2018-10-23 18:42:56 +0800

    automation

--- configure.sh
+++ configure.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-
+echo -e ‘’;
+echo -e ‘’;
+echo -e ‘’;
+echo -e ‘----------------------------install lib’;
 echo -e ‘\n\n\n\n\n aliyun mirror’;
 mv ~/.m2/settings.xml ~/.m2/settings.xml_bak
 cp -rfv ./settings.xml ~/.m2/settings.xml
@@ -24,17 +27,25 @@ mvn clean install -DskipTests -U;
 echo -e ‘\n\n\n\n\n’;
 sleep 2;
 
+echo -e ‘’;
+echo -e ‘’;
+echo -e ‘’;
+echo -e ‘----------------------------rabbitmq’;
+echo -e ‘stop rabbitmq-server’;
+sleep 2;
+echo -e ‘start rabbitmq-server on 5672 at background’;
+rabbitmq-server start &
+sleep 5;
 
-
-echo -e ‘\n\n\n\n\n’;
+echo -e ‘’;
+echo -e ‘’;
+echo -e ‘’;
+echo -e ‘----------------------------redis’;
 echo -e ‘restart redis on 6379’;
-
 sudo redis-cli -p 6379 shutdown;
 sleep 1;
-
 sudo mkdir -p /opt;
 sudo cp -rfv redis/*.* /opt/;
 sudo redis-server /opt/redis_6379.conf;
 sleep 1;
 echo -e ‘\n\n\n\n\n’;
-
