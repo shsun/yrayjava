dd9c0f89e45272c225ae10bf6965f05a6b140f47
sunshanghai <sunshanghai@liquidnetwork.com>
2018-10-26 18:13:03 +0800

    adjust pkg

--- spring-cloud-gateway-route/src/main/java/spring/cloud/gateway/route/GatewayApplication.java
+++ spring-cloud-gateway-route/src/main/java/spring/cloud/gateway/route/GatewayApplication.java
@@ -1,4 +1,4 @@
-package org.xujin.sc.gateway;
+package spring.cloud.gateway.route;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
