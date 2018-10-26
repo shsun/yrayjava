c90afecf002db09acbe09aa17a6dd556dbc48bfd
sunshanghai <sunshanghai@liquidnetwork.com>
2018-10-26 20:13:36 +0800

    introduce eureka for new gateway

--- spring-cloud-gateway-route/src/main/java/spring/cloud/gateway/route/GatewayApplication.java
+++ spring-cloud-gateway-route/src/main/java/spring/cloud/gateway/route/GatewayApplication.java
@@ -2,11 +2,13 @@ package spring.cloud.gateway.route;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 }
